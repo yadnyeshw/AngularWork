be7b612b4b1025302d2e456c688a9c079460c919
yadnyeshw <yadnyeshw@bankerstoolbox.com>
2018-10-05 20:03:40 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from "app/products/product-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ProductListComponent]
 })
 export class AppModule { }
